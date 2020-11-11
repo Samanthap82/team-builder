26cbd8dd7b636b09de9f607825d091a93edbef97
Samantha Prewitt <67523846+Samanthap82@users.noreply.github.com>
2020-11-10 20:42:32 -0600

    done

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Form from "./Form";
+import Teammate from "./Teammate";
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: "",
+};
+// Step 1 We need to State to hold all values of the form
+// fix this using the state hook
 
 function App() {
+  const [team, setTeam] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
+  //  It takes in the name of an input and its value, and updates `formValues`
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  };
+
+  const submitForm = () => {
+    const newTeammate = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+    if (!newTeammate.name || !newTeammate.email || !newTeammate.role) {
+      return;
+    }
+
+    setTeam([...team, newTeammate]);
+    setFormValues(initialFormValues);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Create Your Team</h1>
+      {/* STEP 2 - The form component needs its props. */}
+      {/* //  Check implementation of Form
+        //  to see what props it expects. */}
+      <Form values={formValues} update={updateForm} submit={submitForm} />
+      {team.map((teammate) => {
+        return <Teammate key={teammate.id} details={teammate} />;
+      })}
     </div>
   );
 }
--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,75 +1,73 @@
-import React from 'react'
+import React from "react";
 
-export default function Form(props){
-    const {values, update, submit}= props
-}
+export default function Form(props) {
+  const { values, update, submit } = props;
 
+  // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
+  // a) pull the name of the input from the event object
+  // b) pull the value of the input from the event object
 
- // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
-    // a) pull the name of the input from the event object
-    // b) pull the value of the input from the event object
-
-    // c) use the `update` callback coming in through props
-const onChange = (evt) => {
-    // part a and b 
-    const {name, value} = evt.target; 
+  // c) use the `update` callback coming in through props
+  const onChange = (evt) => {
+    // part a and b
+    const { name, value } = evt.target;
     // part c
-    update(name,value);
-};
- // 🔥 STEP 7 - IMPLEMENT the submit handler
-    // a) don't allow the browser to reload!
-    // / b) use the `submit` callback coming in through props
-    const onSubmit = (evt) => {
-        // step a
-        evt.preventDefault();
-        // step b
-        submit();
-    };
+    update(name, value);
+  };
+  // 🔥 STEP 7 - IMPLEMENT the submit handler
+  // a) don't allow the browser to reload!
+  // / b) use the `submit` callback coming in through props
+  const onSubmit = (evt) => {
+    // step a
+    evt.preventDefault();
+    // step b
+    submit();
+  };
 
-return(
-<form className="form container" onSubmit={onSubmit}>
-<div className="form-group inputs">
+  return (
+    <form className="form container" onSubmit={onSubmit}>
+      <div className="form-group inputs">
+        {/* STEP 3 - Make an input of type `text` for username.
+             Controlled inputs need `value` and `onChange` props.Inputs render what they're told - their current value comes from app state.
+            At each keystroke, a change handler fires to change app state.   */}
 
-{/* // STEP 3 - Make an input of type `text` for username.
-//               Controlled inputs need `value` and `onChange` props.
-//               Inputs render what they're told - their current value comes from app state.
-//               At each keystroke, a change handler fires to change app state.  */}
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            onChange={onChange}
+            value={values.username}
+            placeholder="type a Name"
+            maxLength="30"
+          />
+        </label>
 
-<label>Username
-<input 
-type="text"
-name="username"
-onChange={onChange}
-vaule={values.username.}
-placeholder="type a Name"
-maxLength="30"
-></input>
-</label>
-
-
-{/* STEP 4 - Make an input of type `email` or `text` for email. */}
-<label>
-    Email
-    <input
-    type="email"
-    name="email"
-    onChange={onChange}
-    value={value.email}>
-        </input>
-    </label>
-{/* STEP 5 - Make dropdown for role. */}
-    <label>
-        Role
-        <select name="role" value={values.role}
-        onchange={onChange}>
+        {/* STEP 4 - Make an input of type `email` or `text` for email. */}
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            onChange={onChange}
+            value={values.email}
+          />
+        </label>
+        {/* STEP 5 - Make dropdown for role. */}
+        <label>
+          Role
+          <select name="role" value={values.role} onchange={onChange}>
             <option value="Tank">Tank/</option>
+
             <option value="Healer">Healer</option>
-            <option value="Damage">Damge</option>
-        </select>
-    </label>
-<div className="submit">
-    <button>submit</button>
-</div>
-</div>
-</form>
-);
+
+            <option value="Damage">Damage</option>
+          </select>
+        </label>
+        <div className="submit">
+          <button>submit</button>
+        </div>
+      </div>
+    </form>
+  );
+}
